8b4525c19bf9a5477cfa66acd69b1eadcc932207
Rodrigo Freitas <rodrigo.d.freitas@outlook.com>
2026-04-10 18:04:05 -0300

    style(bio)
    Foto do perfil

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Bio } from './components/bio/bio';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [RouterOutlet, Bio],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/components/bio/bio.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bio',
+  standalone: true,
+  imports: [],
+  templateUrl: './bio.html',
+  styleUrl: './bio.scss',
+})
+export class Bio {
+
+}
